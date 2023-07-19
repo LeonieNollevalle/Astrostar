3d9b92e098c3bbdc26d8b9a2fa451c3bad4db9da
LeonieNollevalle <leonie.nollevalle@gmail.com>
2023-07-19 17:53:54 +0200

    add title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <iframe width="375" height="667" src="https://xd.adobe.com/embed/0d49f3a8-7206-4aea-8469-d54ca41cb8d9-581a/" frameborder="0" allowfullscreen></iframe>
+      <iframe width="375" title="moi" height="667" src="https://xd.adobe.com/embed/0d49f3a8-7206-4aea-8469-d54ca41cb8d9-581a/" frameborder="0" allowfullscreen></iframe>
     </div>
   );
 }
